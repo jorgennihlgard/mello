a68062fbcf02430319e0870a23c293fcb28a2f18
jorgennihlgard <j.nihlgard@outlook.com>
2018-06-14 08:41:35 +0200

    Update connection.php

--- public/connection.php
+++ public/connection.php
@@ -1,5 +1,5 @@
 <?php
-$connection = mysqli_connect("localhost", "tbevjqfq_jorgen", "Jn204080M", "tbevjqfq_mello");
+$connection = mysqli_connect("localhost", "tbevjqfq_jorgen", "miss_inlogg", "tbevjqfq_mello");
 
 mysqli_set_charset($connection,"utf8");
  ?>
